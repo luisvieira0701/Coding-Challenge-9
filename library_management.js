fd8036fefe4349ecc3ccdc3b8cca1e515a2d6f5f
LuisVieira0701 <luisvieira@usf.edu>
2024-10-20 00:54:47 -0400

    T2- Section class to manage books and availability

--- library_management.js
+++ library_management.js
@@ -1,7 +1,7 @@
 //Task 1- Creating book class with properties and methods
 class Book {
     constructor (title, author, ISBN, _isAvailable) {
-        this.tile = title
+        this.title = title
         this.author = author
         this.ISBN = ISBN
         this._isAvailable = true
@@ -11,11 +11,36 @@ class Book {
         console.log(`BookTitle: ${this.title}, BookAuthor: ${this.author}, ISBN: ${this.ISBN}`)
     }
 
-    get _isAvailable () {  //getter to access book current status
-       return {this._isAvailable}
+    get isAvailable () {  //getter to access book current status
+       return this._isAvailable
     }
 
-    set _isAvailable (status) {  //setter to update books availability
-        this.isAvailable = status
+    set isAvailable (status) {  //setter to update books availability
+        this._isAvailable = status
     }
+}
+
+//Task 2- Creating section class to manage books and availability
+class Section {
+    constructor (name, books = []) {
+        this.name = name
+        this.books = books
+    }
+
+    addBook (book) {
+        this.books.push(book)
+    }
+
+    getAvailableBooks () { //Counting how many available books there are
+        let availableBooks = this.books.filter(book => book.isAvailable === true)
+        return availableBooks.length
+    }
+
+    listBooks() {
+        return this.books.map (book => ({ //Reiterating each book's title and availability
+            title: book.title,
+            available: book.isAvailable
+        }));
+
+        }
 }
